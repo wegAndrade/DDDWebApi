1224b9447ffe5e8114772a80541be0a0cb2820d5
Wellington <weg.andrade@gmail.com>
2021-10-09 20:49:35 -0300

    Adicionando camada de Domain

---
+++ DDDWebApi/3.0 Domain/DDDWebApi.Domain/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDDWebApi.Domain.Models
+{
+    public class Cliente : Base
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
---
+++ DDDWebApi/3.0 Domain/DDDWebApi.Domain/Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDDWebApi.Domain.Models
+{
+    public class Produto : Base
+    {
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+        public bool Disponivel { get; set; }
+        public int ClienteId { get; set; }
+        public virtual Cliente Cliente { get; set; }
+    }
+}
