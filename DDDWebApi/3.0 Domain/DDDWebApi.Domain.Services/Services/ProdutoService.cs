131ae3470eec94e56662c589a520b1f2a08e7a16
Wellington <weg.andrade@gmail.com>
2021-10-09 21:01:29 -0300

    Adicionando Serviços da aplicação

---
+++ DDDWebApi/3.0 Domain/DDDWebApi.Domain.Services/Services/ProdutoService.cs
@@ -0,0 +1,13 @@
+﻿using DDDWebApi.Domain.Core.Interfaces.Repositories;
+using DDDWebApi.Domain.Models;
+
+
+namespace DDDWebApi.Domain.Services.Services
+{
+    public class ProdutoService : ServiceBase<Produto>
+    {
+        public ProdutoService(IRepositoryProduto RepositoryProduto) : base(RepositoryProduto)
+        {
+        }
+    }
+}
---
+++ DDDWebApi/3.0 Domain/DDDWebApi.Domain.Services/Services/ServiceCliente.cs
@@ -0,0 +1,18 @@
+﻿using DDDWebApi.Domain.Core.Interfaces.Repositories;
+using DDDWebApi.Domain.Core.Interfaces.Services;
+using DDDWebApi.Domain.Models;
+
+namespace DDDWebApi.Domain.Services.Services
+{
+    public class ServiceCliente : ServiceBase<Cliente>, IServiceCliente
+    {
+        public readonly IRepositoryCliente _repositoryCliente;
+
+        public ServiceCliente(IRepositoryCliente RepositoryCliente)
+            : base(RepositoryCliente)
+        {
+            _repositoryCliente = RepositoryCliente;
+        }
+
+    }
+}
