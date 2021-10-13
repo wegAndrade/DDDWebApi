0986e0af6105b3406641e1b1c8f1d4f0fa7a3e7e
Wellington <wellington.silva@gomara.tech>
2021-10-13 16:58:19 -0300

    Adicionando camada de presentation

---
+++ DDDWebApi/1.0 Presentation/DDDWebApi.Presentation/Controllers/ProdutosController.cs
@@ -0,0 +1,97 @@
+﻿using DDDWebApi.Application.DTO.DTO;
+using DDDWebApi.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DDDWebApi.Presentation.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly IApplicationServiceProduto _applicationServiceProduto;
+
+        public ProdutosController(IApplicationServiceProduto ApplicationServiceProduto)
+        {
+            _applicationServiceProduto = ApplicationServiceProduto;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(_applicationServiceProduto.GetAll());
+        }
+
+        // GET api/values/5\
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return Ok(_applicationServiceProduto.GetById(id));
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult Post([FromBody] ProdutoDTO produtoDTO)
+        {
+            try
+            {
+                if (produtoDTO == null)
+                    return NotFound();
+
+
+                _applicationServiceProduto.Add(produtoDTO);
+                return Ok("O produto foi cadastrado com sucesso");
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        // PUT api/values/5
+        [HttpPut]
+        public ActionResult Put([FromBody] ProdutoDTO produtoDTO)
+        {
+
+            try
+            {
+                if (produtoDTO == null)
+                    return NotFound();
+
+                _applicationServiceProduto.Update(produtoDTO);
+                return Ok("O produto foi atualizado com sucesso!");
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        // DELETE api/values/5
+        [HttpDelete()]
+        public ActionResult Delete([FromBody] ProdutoDTO produtoDTO)
+        {
+            try
+            {
+                if (produtoDTO == null)
+                    return NotFound();
+
+                _applicationServiceProduto.Remove(produtoDTO);
+                return Ok("O produto foi removido com sucesso!");
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
