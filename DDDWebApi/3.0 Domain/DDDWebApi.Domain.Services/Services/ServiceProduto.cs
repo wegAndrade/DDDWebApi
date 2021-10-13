8b79e422bd57234d7da711d4759c53edf34ca888
Wellington <wellington.silva@gomara.tech>
2021-10-13 16:57:13 -0300

    Ajustando nome do serviço de produtos

---
+++ DDDWebApi/3.0 Domain/DDDWebApi.Domain.Services/Services/ServiceProduto.cs
@@ -0,0 +1,14 @@
+﻿using DDDWebApi.Domain.Core.Interfaces.Repositories;
+using DDDWebApi.Domain.Core.Interfaces.Services;
+using DDDWebApi.Domain.Models;
+
+
+namespace DDDWebApi.Domain.Services.Services
+{
+    public class ServiceProduto : ServiceBase<Produto>, IServiceProduto
+    {
+        public ServiceProduto(IRepositoryProduto RepositoryProduto) : base(RepositoryProduto)
+        {
+        }
+    }
+}
